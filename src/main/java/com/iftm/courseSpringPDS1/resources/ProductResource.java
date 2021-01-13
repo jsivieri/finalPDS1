a2689b6420a890bdf23c0fb60ac4ff4999eda54f
JoaoMarcos <juka_jms@hotmail.com>
2021-01-13 06:48:21 -0300

    Custom validation response

--- src/main/java/com/iftm/courseSpringPDS1/resources/ProductResource.java
+++ src/main/java/com/iftm/courseSpringPDS1/resources/ProductResource.java
@@ -1,14 +1,21 @@
 package com.iftm.courseSpringPDS1.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.iftm.courseSpringPDS1.dto.ProductCategoriesDTO;
 import com.iftm.courseSpringPDS1.entities.Product;
 import com.iftm.courseSpringPDS1.services.ProductService;
 
@@ -32,6 +39,25 @@ public class ProductResource {
 		Product obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@PostMapping
+	public ResponseEntity<ProductDTO> insert(@RequestBody ProductCategoriesDTO dto) {
+		ProductDTO newDto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+					.buildAndExpand(newDto.getId()).toUri();
+		return ResponseEntity.created(uri).body(newDto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductCategoriesDTO dto) {
+		ProductDTO newDto = service.update(id, dto);
+		return ResponseEntity.ok().body(newDto);
 
 }
 
--- src/main/java/com/iftm/courseSpringPDS1/services/ProductService.java
+++ src/main/java/com/iftm/courseSpringPDS1/services/ProductService.java
@@ -2,10 +2,21 @@ package com.iftm.courseSpringPDS1.services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.iftm.courseSpringPDS1.dto.ProductCategoriesDTO;
+import com.iftm.courseSpringPDS1.entities.Category;
 import com.iftm.courseSpringPDS1.entities.Product;
+import com.iftm.courseSpringPDS1.repositories.CategoryRepository;
 import com.iftm.courseSpringPDS1.repositories.ProductRepository;
+import com.iftm.courseSpringPDS1.services.exceptions.DatabaseException;
+import com.iftm.courseSpringPDS1.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ProductService {
@@ -13,13 +24,68 @@ public class ProductService {
 	@Autowired
 	private ProductRepository repository;
 	
-	public List<Product> findAll() {
-		return repository.findAll();
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	public List<ProductDTO> findAll() {
+		List<Product> list =  repository.findAll();
+		return list.stream().map(e -> new ProductDTO(e)).collect(Collectors.toList());
+		
+	public ProductDTO findById(Long id) {
+		Optional<Product> list = repository.findAll();
+		return list.stream().map(e -> new ProductDTO(e)).collect(Collectors.toList());
+	}
+		
+		
+	@Transactional
+	public ProductDTO insert(ProductCategoriesDTO dto) {
+		Product entity = dto.toEntity();
+		setProductCategories(entity, dto.getCategories());
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+	}
+
+	private void setProductCategories(Product entity, List<CategoryDTO> categories) {
+		entity.getCategories().clear();
+		for (CategoryDTO dto : categories) {
+			Category category = categoryRepository.getOne(dto.getId());
+			entity.getCategories().add(category);
+		}
 	}
 	
-	public Product findById(Long id) {
-		Optional<Product> obj = repository.findById(id);
-		return obj.get();
+	@Transactional
+	public ProductDTO update(Long id, ProductCategoriesDTO dto) {
+		try {
+			Product entity = repository.getOne(id);
+			updateData(entity, dto);
+			entity = repository.save(entity);
+			return new ProductDTO(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage()); 
+		}
+	}
+	
+	private void updateData(Product entity, ProductCategoriesDTO dto) {
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());
+		entity.setImgUrl(dto.getImgUrl());
+		if (dto.getCategories() != null && dto.getCategories().size() > 0) {
+			setProductCategories(entity, dto.getCategories());
+		}
+	}
+	
+	
 
 }
